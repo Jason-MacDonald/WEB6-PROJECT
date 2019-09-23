002aa28c3bdf50d8104342b5339ea84996acfede
Jason-MacDonald <jason-macdonald@outlook.co.nz>
2019-09-23 17:55:01 +1200

    Added POST - addVehicle

--- api/apis.js
+++ api/apis.js
@@ -9,6 +9,20 @@ function vehicleList(req, res) {
     res.end(JSON.stringify(data));
 };
 
+function addVehicle(req, res) {
+    let body = '';
+    req.on('data', chunk => body += chunk.toString());
+    req.on('end', () => {
+        data.push(JSON.parse(body));
+        res.statusCode = 201;
+        return res.end(`Added ${JSON.parse(body).make}`);
+    });
+    req.on('error', error => {
+        res.statusCode = 400;
+        return res.end(error);
+    });
+}
+
 function errorRequest(req, res) {
     res.statusCode = 404;
     res.end(`This api is not supported!`);
@@ -21,6 +35,9 @@ const server = http.createServer((req, res) => {
         switch (req.method) {
             case 'GET':
                 vehicleList(req, res);
+                break;
+            case 'POST':
+                addVehicle(req, res);
                 break;
             default:
                 errorRequest(req, res);
