04fcbd39dbf37738dae51f6513e33b6246bca457
Jason-MacDonald <jason-macdonald@outlook.co.nz>
2019-09-28 21:10:27 +1200

    Modified search form component to use appropriate data sources.

--- src/components/search-form/search-form.js
+++ src/components/search-form/search-form.js
@@ -2,73 +2,109 @@ import React from 'react';
 
 import './search-form.css'
 
+// Setup for dropdowns
+const dataSource = [
+    ['Any Make', []],
+    ['Audi', ['A4', 'TT']],
+    ['BMW', ['330i']], 
+    ['Chevrolet', ['Sonic']],
+    ['Ford', ['Courier', 'Ecosport', 'Fairmont', 'Falcon', 'Kuga', 'Laser', 'Ranger', 'Transit']], 
+    ['Holden'], 
+    ['Honda'], 
+    ['Hyundai'], 
+    ['LDV'], 
+    ['Lexus'], 
+    ['Mazda'], 
+    ['Mitsubishi']
+];
+
+const makeItems = [];
+const modelItems = [];
+
+let selectedMake = 4; //TODO: Update index depending on make chosen in drop down.
+
+// eslint-disable-next-line
+for(const [index, value] of dataSource.entries()) {
+    makeItems.push(<option key={index}>{value[0]}</option>)
+}
+// eslint-disable-next-line
+for(const [index, value] of dataSource[selectedMake][1].entries()) {
+    modelItems.push(<option key={index}>{value}</option>)
+}
+// eslint-disable-next-line
+for(const [index, value] of dataSource.entries()) {
+    makeItems.push(<option key={index}>{value[0]}</option>)
+}
+
 class SearchForm extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-            
-    //     }
-    // }
     render () {
+
+        const bodyOptions = ['Any Body', 'Convertable', 'Coupe', 'Hatchback', 'Sedan', 'Station Wagon', 'RV/SUV', 'Ute', 'Van'];
+        
+        const yearOptions = [];
+        const currentYear = new Date().getFullYear();
+        for (let i = (currentYear); i > (currentYear - 50); i--) {
+            yearOptions.push(i);
+        }
+
+        const priceOptions = [];
+        for (let i = 1; i < 76; i += 2) {
+            priceOptions.push(i + 'k');
+        }
+
         return (
             <div className='search-form-main-div'>
                 <form>
                     <h2>Find a Car</h2>
                     <input type='text' placeholder='Keywords...'/>
-                    {/* Add selects from other file */}
-                    <select>
-                        <option>Any Make</option>
-                        <option>Make 1</option>
-                        <option>Make 2</option>
-                        <option>Make 3</option>
+                    <select id='DDmake'>
+                        {makeItems};
                     </select>
                     <select>
                         <option>Any Model</option>
-                        <option>Make 1</option>
-                        <option>Make 2</option>
-                        <option>Make 3</option>
+                        {modelItems};
                     </select>
                     <select>
-                        <option>Any Body Style</option>
-                        <option>Make 1</option>
-                        <option>Make 2</option>
-                        <option>Make 3</option>
+                        {bodyOptions.map((value, index) => {
+                            return <option key={index}>{value}</option>
+                        })}
                     </select>
                     <select>
-                        <option>Any Model</option>
+                        <option>Any Transmission</option>
                         <option>Automatic</option>
                         <option>Manual</option>
+                        <option>Tiptronic</option>
                     </select>
                     <select className="inline-block">
                         <option>Any Price</option>
-                        <option>1k</option>
-                        <option>2k</option>
+                        {priceOptions.map((value, index) => {
+                            return <option key={index}>{value}</option>
+                        })}
                     </select>
 
                     <p className="inline-block">to</p>
 
                     <select className="inline-block">
                         <option>Any Price</option>
-                        <option>1k</option>
-                        <option>2k</option>
+                        {priceOptions.map((value, index) => {
+                            return <option key={index}>{value}</option>
+                        })}
                     </select>
 
                     <select className="inline-block">
                         <option>Any Year</option>
-                        <option>2019</option>
-                        <option>2018</option>
-                        <option>2017</option>
-                        <option>2016</option>
+                        {priceOptions.map((value, index) => {
+                            return <option key={index}>{value}</option>
+                        })}
                     </select>
 
                     <p>to</p>
                     
                     <select className="inline-block">
-                        <option>Any Year</option>
-                        <option>2019</option>
-                        <option>2018</option>
-                        <option>2017</option>
-                        <option>2016</option>
+                    <option>Any Year</option>
+                        {yearOptions.map((value, index) => {
+                            return <option key={index}>{value}</option>
+                        })}
                     </select>
                     <input className="submit" type="submit" value="Find Car"/>
                 </form>
