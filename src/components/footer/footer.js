9e79b3a850341ec5e02c8438846a746d8d2626a7
Jason-MacDonald <jason-macdonald@outlook.co.nz>
2019-09-29 16:06:59 +1300

    Modified footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,43 +7,36 @@ const Footer = () => {
         <div className="footer-main-div">
             <div className='footer-contact'>
                 <h2>Contact</h2>
-                <Contact />
-                <Contact />
+                <Contact s/>
                 <Contact />
             </div>
 
             <div className='footer-opening-hours'>
                 <h2>Opening Hours</h2>
-                <div>
-                    <p>Monday</p>
-                    <p>Tuesday</p>
-                    <p>Wednesday</p>
-                    <p>Thursday</p>
-                    <p>Friday</p>
-                    <p>Saturday</p>
-                    <p>Sunday</p>
-                </div>
-                <div>
-                    <p>8:00 - 5:30</p>
-                    <p>8:00 - 5:30</p>
-                    <p>8:00 - 5:30</p>
-                    <p>8:00 - 5:30</p>
-                    <p>8:00 - 5:30</p>
-                    <p>8:00 - 5:30</p>
-                    <p>8:00 - 5:30</p>
+                <div className="hours">
+                    <p><strong>Monday</strong> &emsp; 8:00 - 5:30</p>
+                    <p><strong>Tuesday</strong> &emsp; 8:00 - 5:30</p>
+                    <p><strong>Wednesday</strong> &emsp; 8:00 - 5:30</p>
+                    <p><strong>Thursday</strong> &emsp; 8:00 - 5:30</p>
+                    <p><strong>Friday</strong> &emsp; 8:00 - 5:30</p>
+                    <p><strong>Saturday</strong> &emsp; 8:00 - 5:30</p>
+                    <p><strong>Sunday</strong> &emsp; 8:00 - 5:30</p>
                 </div>
             </div>
             <div className='footer-locations'>
                 <h2 >Locations</h2>
-
-                <h3>Nelson</h3>
-                <p>123 Nelson Street</p>
-
-                <h3>Tahunanui</h3>
-                <p>123 Tahunanui Street</p>
-
-                <h3>Richmond</h3>
-                <p>123 Richmond Street</p>
+                <div className="location">
+                    <h3>Nelson</h3>
+                    <p>123 Nelson Street</p>
+                </div>
+                <div className="location">
+                    <h3>Tahunanui</h3>
+                    <p>123 Tahunanui Street</p>
+                </div>
+                <div className="location">
+                    <h3>Richmond</h3>
+                    <p>123 Richmond Street</p>
+                </div>
             </div>
         </div>
     )
@@ -53,7 +46,7 @@ const Footer = () => {
 class Contact extends React.Component {
     render() {
         return (
-            <div>
+            <div className="contact">
                 <img alt='Nothing Yet' />
                 <p>John Smith</p>
                 <p>021 123 456</p>
