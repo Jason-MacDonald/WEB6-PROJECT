ada1e139d881f235d98a1e9e8e7cbaa58d07ba98
Jason-MacDonald <jason-macdonald@outlook.co.nz>
2019-11-01 23:27:59 +1300

    Filtered results showing

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,17 +1,17 @@
-import { ADD_VEHICLE } from '../constants/index'
-import { DATA_LOADED } from '../constants/index'
+import { ADD_VEHICLE, DATA_LOADED, } from '../constants/index'
 
 export function addVehicle(payload) {
     return { type: ADD_VEHICLE, payload };
 }
 
 export function getData() {
-    // Change to get from store.
     return function (dispatch) {
         return fetch('http://localhost:4200/api/vehicles')
             .then(res => res.json())
             .then(json => {
-                dispatch ({ type: DATA_LOADED, payload: json });
+                dispatch({ type: DATA_LOADED, payload: json });
             });
     };
 }
+
+
